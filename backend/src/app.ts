8df66f397425918d22295a13349b694608cd6835
Eric Radja <eric.raj12@gmail.com>
2020-12-21 14:42:50 +0300

    Setup backend

---
+++ backend/src/app.ts
@@ -0,0 +1,30 @@
+import "dotenv-safe/config";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+const main = async () => {
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5000;
+
+  const app = express();
+
+  app.use(cors());
+
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // parse application/json
+  app.use(bodyParser.json());
+
+  app.use("/", (_req, res) => {
+    res.send("Hello World !");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is ready on ${PORT}`);
+  });
+};
+
+main().catch((error) => {
+  console.log("error :>> ", error);
+});
